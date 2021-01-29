d4b9bbd5b4d575609670b05a6509f12ed01cffb5
warren clark <wclarkcito@gmail.com>
2021-01-29 12:55:12 -0800

    added searchbar jumbotron and started forming columns

---
+++ assets/script.js
@@ -0,0 +1,11 @@
+var row = $("<div class = 'row'>");
+var columnOne = ("<div class = 'col-md-12'>")
+
+
+//displays current date and time
+
+var dt = new Date(); var time = dt
+var currentdate = new Date(); var datetime = "Now: " + currentdate;
+
+
+$("#currentDay").html(currentdate)
