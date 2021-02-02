4898300316d4646beff96d8f0bfc798b0bbd1ae3
warren clark <wclarkcito@gmail.com>
2021-02-02 13:59:33 -0800

    created divs to display the weather humidity UV index etc.  accidentally created an infinite loop and am working on restoring access to API

--- assets/script.js
+++ assets/script.js
@@ -6,7 +6,7 @@ var uvIndex = "";
 var submitCity = $('#submit-city');
 
 var forecastURL = 'http://api.openweathermap.org/data/2.5/forecast?q=';
-var apiKey = '643023c5499bd5e2ce5d5d93c212f70c';
+var apiKey = '7bb91bb18b7935d889972f049c9ce57e';
 var appId = '&appid=';
 //fetch request for url
 
@@ -16,6 +16,12 @@ submitCity.on('click', function (event) {
     console.log(event)
     event.preventDefault();
     var searchInput = $('.form-control').val();
+    displayWeather(searchInput)
+
+});
+function displayWeather(searchInput) {
+
+
     var submitURL = forecastURL + searchInput + appId + apiKey;
     //console.log(submitURL)
 
@@ -31,7 +37,7 @@ submitCity.on('click', function (event) {
                 url: weatherURL,
                 method: 'GET',
             }).then(res => {
-                console.log(res);
+                console.log("current day", res);
                 currentDay = res;
                 $.ajax({
                     url: `http://api.openweathermap.org/data/2.5/uvi?lat=${res.lat}&lon=${res.lon}&appId=${apiKey}`
@@ -48,8 +54,8 @@ submitCity.on('click', function (event) {
         })
 
 
-})
-
+}
+//local storage
 function handleHistory(value) {
     var tempHistory = JSON.parse(window.localStorage.getItem("history"));
     console.log(tempHistory)
@@ -61,19 +67,43 @@ function handleHistory(value) {
             date: new Date().getTime()
         });
         window.localStorage.setItem("history", JSON.stringify(tempHistory))
-        handleHistory(value)
+
     } else {
+        var historyFlag = false;
+        tempHistory.forEach((query, queryIndex) => {
+            if (query.city.toLowerCase() === value.toLowerCase()) {
+                historyFlag = true;
+                tempHistory[queryIndex].date = new Date().getTime();
+                window.localStorage.setItem("history", JSON.stringify(tempHistory));
+            }
+
+        });
+        if (!historyFlag) {
+            tempHistory.push({ city: value, date: new Date().getTime() });
+            window.localStorage.setItem("history", JSON.stringify(tempHistory));
+        }
         var list = document.getElementById("city");
+        list.innerHTML = "";
         tempHistory.forEach((item, index) => {
             var listItem = document.createElement("li");
             listItem.classList.add("item-history");
             listItem.innerHTML = item.city;
+            listItem.addEventListener("click", e => displayWeather(item.city));
             list.appendChild(listItem)
 
         })
     }
 }
+function displayCurrentWeather(data) {
+    var date = document.getElementById("weather-date");
+    var img = document.getElementById("weather-img");
+    var temp = document.getElementById("weather-temp");
+    var humidity = document.getElementById("weather-humidity");
+    var windspeed = document.getElementById("weather-speed");
+    var uv = document.getElementById("weather-uv");
+    img.src = "http://openweathermap.org/img/w/" + /*iconcode*/ + ".png";
 
+}
 
 
 
