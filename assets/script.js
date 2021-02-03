981d467d4b763d3584831e8367ba46a09fb1881e
warren clark <wclarkcito@gmail.com>
2021-02-03 14:21:25 -0800

    functionality is completed added CSS styling to the 5 day forecast and list items for search histroy

--- assets/script.js
+++ assets/script.js
@@ -1,11 +1,11 @@
-//variable for url
+//Global Variables
 var fiveDay = "";
 var currentDay = "";
 var uvIndex = "";
 
 var submitCity = $('#submit-city');
 
-var forecastURL = 'http://api.openweathermap.org/data/2.5/forecast?q=';
+var forecastURL = 'http://api.openweathermap.org/data/2.5/forecast?units=imperial&q=';
 var apiKey = '643023c5499bd5e2ce5d5d93c212f70c';
 var appId = '&appid=';
 
@@ -32,13 +32,16 @@ function displayWeather(searchInput) {
     })
         .then(function (res) {
             fiveDay = res;
+
             console.log(res);
-            var weatherURL = `https://api.openweathermap.org/data/2.5/onecall?lat=${res.city.coord.lat}&lon=${res.city.coord.lon}&appId=${apiKey}`
+            var weatherURL = `https://api.openweathermap.org/data/2.5/onecall?units=imperial&lat=${res.city.coord.lat}&lon=${res.city.coord.lon}&appId=${apiKey}`
             $.ajax({
                 url: weatherURL,
                 method: 'GET',
             }).then(res => {
                 console.log("current day", res);
+                displayCurrentWeather(res.current, searchInput);
+                displayFiveDay(res.daily);
                 currentDay = res;
                 $.ajax({
                     url: `http://api.openweathermap.org/data/2.5/uvi?lat=${res.lat}&lon=${res.lon}&appId=${apiKey}`
@@ -95,19 +98,57 @@ function handleHistory(value) {
         })
     }
 }
-function displayCurrentWeather(data) {
+
+//function to display current weather
+
+function displayCurrentWeather(data, city) {
     var date = document.getElementById("weather-date");
     var img = document.getElementById("weather-img");
     var temp = document.getElementById("weather-temp");
     var humidity = document.getElementById("weather-humidity");
     var windspeed = document.getElementById("weather-speed");
     var uv = document.getElementById("weather-uv");
-    img.src = "http://openweathermap.org/img/w/" + /*iconcode*/ + ".png";
-
+    img.src = "http://openweathermap.org/img/w/" + data.weather[0].icon + ".png";
+    date.innerHTML = `${city} (${formatDate(new Date(data.dt * 1000))})`;
+    temp.innerHTML = `${data.temp}&deg;F`;
+    humidity.innerHTML = `${data.humidity}%`;
+    windspeed.innerHTML = `${data.wind_speed}mph`;
+    uv.innerHTML = data.uvi;
 }
+//function to display the five day forecast
 
-
-
+function displayFiveDay(data) {
+    var container = document.getElementById("five-day");
+    container.innerHTML = ""
+    if (data.length > 0) {
+        data.forEach((day, index) => {
+            if (index > 0 && index < 6) {
+                var listItem = document.createElement("li");
+                var date = document.createElement("div");
+                var img = document.createElement("img");
+                var temp = document.createElement("div");
+                var humidity = document.createElement("div");
+                listItem.appendChild(date);
+                listItem.appendChild(img);
+                listItem.appendChild(temp);
+                listItem.appendChild(humidity);
+                container.appendChild(listItem);
+                date.innerHTML = formatDate(new Date(day.dt * 1000))
+                img.src = "http://openweathermap.org/img/w/" + day.weather[0].icon + ".png";
+                temp.innerHTML = `Temp: ${day.temp.day}&deg;F`
+                humidity.innerHTML = `humidity: ${day.humidity}%`
+            }
+        })
+    }
+}
+function formatDate(date) {
+    var month = date.getMonth();
+    var day = date.getDate();
+    var year = date.getFullYear();
+    month = month < 9 ? `0${month + 1}` : month;
+    var displayDate = `${month}/${day}/${year}`;
+    return displayDate
+}
 
 
 
